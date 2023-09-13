8c79470ddd5bbe270dcf78b9131c6f0cb06d7daa
nej <greger.grym@gmail.com>
2023-09-13 11:34:36 +0200

    suuii

---
+++ ÖvningUI-Unity/Assets/ButtonAction.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour
+{
+
+    public void DisplayMessage()
+    {
+        Debug.Log(message:"Du har tryckt på knappen");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
